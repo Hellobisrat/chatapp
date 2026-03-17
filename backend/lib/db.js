d4a47df9ae1ae8fdb5ffeec873695250f603f692
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-03-17 13:39:42 -0500

    user model added

---
+++ backend/lib/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+
+const connectDB =async()=>{
+  try{
+    mongoose.connection.on('connected',()=>console.log("Database Connected"))
+    await mongoose.connect(`${process.env.MONGODB_URI}`)
+
+  } catch(error){
+
+    console.log(error)
+    process.exit(1)
+  };
+  
+}
+
+export default connectDB;
